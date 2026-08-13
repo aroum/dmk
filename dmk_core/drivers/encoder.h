ba1a15f72f7de9f97c05fe5b32efb5ed300cd1ee
aroum <aroum24@gmail.com>
2026-08-13 23:24:01 +0300

    fix(encoder): add conditional no-op stubs to encoder.h and refine docs

--- dmk_core/drivers/encoder.h
+++ dmk_core/drivers/encoder.h
@@ -1,7 +1,18 @@
 #ifndef ENCODER_H
 #define ENCODER_H
 
+#include "config.h"
+
+#if defined(ENCODER_PINS_A) && defined(ENCODER_PINS_B)
+
 void encoder_init(void);
 void encoder_scan(void);
+
+#else
+
+static inline void encoder_init(void) {}
+static inline void encoder_scan(void) {}
+
+#endif
 
 #endif // ENCODER_H
